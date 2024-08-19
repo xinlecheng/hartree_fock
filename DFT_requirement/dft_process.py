9c1c8f0d9243971dd82cc2615a9a267d4b2c5b6b
xinlecheng <xinlecheng2019@gmail.com>
2024-08-19 11:05:34 +0200

    update

--- DFT_requirement/dft_process.py
+++ DFT_requirement/dft_process.py
@@ -2,10 +2,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('dis_upper_dir_0', type=float)
 parser.add_argument('dis_upper_dir_1', type=float)
+parser.add_argument('h_mm_scale', type=float, default=1.0)
 args = parser.parse_args()
-h0 = 13.54295
-h_ms = h0*(0.37222232-0.25)
-h_mm = h0*0.5
+h_ms = 1.6680535
+h_mm = 7.268859*args.h_mm_scale
 h = 50.0
 dis_upper_dir = (args.dis_upper_dir_0, args.dis_upper_dir_1)
 with open('./POSCAR', 'w') as file:
